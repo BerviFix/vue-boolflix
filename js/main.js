0c2c9f9a0b61c64513d6a1d12553e02bb40bd7c8
Simone <simone.bervi@gmail.com>
2020-12-18 17:08:26 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+// var app = new Vue({
+//     el: '#root',
+//     data: {
+//         movies: [],
+        
+//     },
+//     methods: {
+
+//     },
+
+//     mounted: function () {
+//         var self = this;
+//         axios
+//             .get('https://api.themoviedb.org/3/search/movie', {
+//                 params: {
+//                     api_key: '4ec44032bc6307e6d5108c85f5016dc7',
+//                     query: 'ritorno%20al%20futuro',
+//                     language: 'it-IT',
+//                 }
+//             })
+//             .then(
+//                 (result) => {
+//                     // self.movies = result.data.response;
+//                     console.log(result);
+//                 });
+//     },
+
+// });
+
