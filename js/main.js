4cb6c5e613a14c260e51c8c9b24866ef2b1bc278
Simone <simone.bervi@gmail.com>
2020-12-18 22:06:13 +0100

    Aggiunta Home dinamica

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+// my API Key 4ec44032bc6307e6d5108c85f5016dc7
+
 var app = new Vue({
     el: '#root',
     data: {
         movies: [],
+        popularMovies: [],
+        votatedMovies: [],
+        comingMovies: [],
         titleSearched: "",
         moviesSearched: [],
         
@@ -9,6 +14,8 @@ var app = new Vue({
     methods: {
         searchMovie: function (){
             var self = this;
+            self.moviesSearched = [];
+            
             axios
                 .get('https://api.themoviedb.org/3/search/movie', {
                     params: {
@@ -20,10 +27,45 @@ var app = new Vue({
                 .then(function (result) {
                     self.movies = result.data.results;
                 });
+            
         },
     },
 
-    mounted: function () {
+    created: function () {
+        var self = this;
+
+        axios
+            .get('https://api.themoviedb.org/3/movie/popular', {
+                params: {
+                    api_key: "4ec44032bc6307e6d5108c85f5016dc7",
+                    language: 'it-IT',
+                }
+            })
+            .then(function (result) {
+                self.popularMovies = result.data.results;
+            });
+
+        axios
+            .get('https://api.themoviedb.org/3/movie/top_rated', {
+                params: {
+                    api_key: "4ec44032bc6307e6d5108c85f5016dc7",
+                    language: 'it-IT',
+                }
+            })
+            .then(function (result) {
+                self.votatedMovies = result.data.results;
+            });
+
+        axios
+            .get('https://api.themoviedb.org/3/movie/upcoming', {
+                params: {
+                    api_key: "4ec44032bc6307e6d5108c85f5016dc7",
+                    language: 'it-IT',
+                }
+            })
+            .then(function (result) {
+                self.comingMovies = result.data.results;
+            });    
     },
 
 });
