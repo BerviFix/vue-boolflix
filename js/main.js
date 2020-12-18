f9e4d5059658dd0b044363bc110471402f7230d4
Simone <simone.bervi@gmail.com>
2020-12-18 20:03:41 +0100

    Milestone 1 conclusa

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
-
-// var app = new Vue({
-//     el: '#root',
-//     data: {
-//         movies: [],
+var app = new Vue({
+    el: '#root',
+    data: {
+        movies: [],
+        titleSearched: "",
+        moviesSearched: [],
         
-//     },
-//     methods: {
+    },
+    methods: {
+        searchMovie: function (){
+            var self = this;
+            axios
+                .get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: "4ec44032bc6307e6d5108c85f5016dc7",
+                        query: self.titleSearched,
+                        language: 'it-IT',
+                    }
+                })
+                .then(function (result) {
+                    self.movies = result.data.results;
+                });
+        },
+    },
 
-//     },
+    mounted: function () {
+    },
 
-//     mounted: function () {
-//         var self = this;
-//         axios
-//             .get('https://api.themoviedb.org/3/search/movie', {
-//                 params: {
-//                     api_key: '4ec44032bc6307e6d5108c85f5016dc7',
-//                     query: 'ritorno%20al%20futuro',
-//                     language: 'it-IT',
-//                 }
-//             })
-//             .then(
-//                 (result) => {
-//                     // self.movies = result.data.response;
-//                     console.log(result);
-//                 });
-//     },
-
-// });
+});
 
